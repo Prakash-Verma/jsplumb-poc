aeb59ff0d6b6043f210877df95227203eee6cf2a
Soni Rani <soni.rani@oncehub.com>
2021-09-21 12:53:33 +0530

    POC Adding endpoints to groups

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input } from '@angular/core';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
-
+import { EndpointOptions } from '@jsplumb/core';
 @Component({
   selector: 'app-group',
   templateUrl: './group.component.html',
@@ -9,11 +9,14 @@ import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 export class GroupComponent implements AfterViewInit {
   @Input() elementId!: string;
   @Input() jsPlumbInstance!: BrowserJsPlumbInstance;
+  @Input() sourceGroup!: ElementRef;
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(public elementRef: ElementRef) {}
 
   ngAfterViewInit() {
     this.addGroupElement();
+    this.addSourceElement();
+    this.addTargetElement();
   }
 
   private addGroupElement() {
@@ -21,6 +24,47 @@ export class GroupComponent implements AfterViewInit {
       el: this.elementRef.nativeElement,
       id: this.elementId,
       droppable: true,
+      constrain: true,
     });
+  }
+
+  private addSourceElement() {
+    const source: EndpointOptions = {
+      endpoint: {
+        type: 'Dot',
+        options: { radius: 5 },
+      },
+      paintStyle: { fill: 'blue' },
+      source: true,
+    };
+    this.jsPlumbInstance.addEndpoint(
+      this.elementRef.nativeElement,
+      {
+        anchor: 'Right',
+        uuid: this.elementId + '_right',
+      },
+      source
+    );
+  }
+
+  private addTargetElement() {
+    const target: EndpointOptions = {
+      endpoint: {
+        type: 'Dot',
+        options: {
+          radius: 5,
+          cssClass: 'target-connection',
+        },
+      },
+      paintStyle: { fill: 'blue' },
+      target: true,
+      maxConnections: 10,
+    };
+
+    this.jsPlumbInstance.addEndpoint(
+      this.elementRef.nativeElement,
+      { anchor: 'AutoDefault', uuid: this.elementId + '_left' },
+      target
+    );
   }
 }
